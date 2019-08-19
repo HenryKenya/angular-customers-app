03c044f03bb0216c030aed4820c56861d8d2f68e
Henry Onyango <henry.onyango@outlook.com>
2019-08-19 19:42:55 +0300

    bind data to customers-list component

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ICustomer } from 'src/app/shared/interfaces';
+
 @Component({
     selector: 'app-customers-list',
     templateUrl: './customers-list.component.html'
@@ -7,11 +9,24 @@ import { Component, OnInit } from '@angular/core';
 
 export class CustomersListComponent implements OnInit {
 
-    filteredCustomers: any[] = [];
+    filteredCustomers: ICustomer[] = [];
     customersOrderTotal: number;
     currencyCode: string = "Ksh";
    
     constructor() {}
     
-    ngOnInit(){ }
+    ngOnInit(){ 
+
+        
+    }
+
+    // calculate orders
+    calculateOrders = () => {
+
+        this.customersOrderTotal = 0;
+
+        this.filteredCustomers.forEach( (customer: ICustomer) => {
+                this.customersOrderTotal += customer.orderTotal
+        })
+    }
 }
