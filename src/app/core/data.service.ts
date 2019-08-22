947b57bae5860477b5742bb24dbc455e20b6a618
Henry Onyango <henry.onyango@outlook.com>
2019-08-22 18:38:25 +0300

    get customers, customer and order in data.service

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -11,8 +11,48 @@ export class DataService {
 
     baseUrl:string = 'assets/';
 
-    constructor() {}
+    constructor( private http: HttpClient ) {}
 
+
+    // get all customers
+    getCustomers() : Observable<ICustomer[]>{
+
+        return this.http.get<ICustomer[]>(`${this.baseUrl}customers.json`)
+                .pipe(
+                    catchError(this.handleError)
+                );
+
+    }
+
+    // get single customer
+    getCustomer(id: number) : Observable<ICustomer> {
+
+        return this.http.get<ICustomer[]>(`${this.baseUrl}customers.json`)
+                .pipe(
+                    map(customers => {
+                        let customer = customers.filter((cust : ICustomer) => cust.id === id);
+                        return (customer && customer.length) ? customer[0] : null;
+                    }),
+                    catchError(this.handleError)
+
+                )
+
+    }
+
+    // get filtered orders
+    getOrders(id: number) : Observable<IOrder[]> {
+        return this.http.get<IOrder[]>(this.baseUrl + 'orders.json')
+          .pipe(
+            map(orders => {
+              let custOrders = orders.filter((order: IOrder) => order.customerId === id);
+              return custOrders;
+            }),
+            catchError(this.handleError)
+          );
+      }
+
+
+    // handle errors in response 
     private handleError = (error: any) => {
         console.log(`server error: ${error}`)
 
